b5221d14a35ba109224399eeae616005ec7dc783
SujibShrestha <sujeeb98@gmail.com>
2026-09-26 09:07:16 +0545

    Add Docker support for frontend and backend services; configure nginx and Dockerfile

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,6 +1,10 @@
 import "dotenv/config";
 import { PrismaClient } from "../generated/prisma/index.js";
 import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig } from "@neondatabase/serverless";
+import WebSocket from "ws";
+
+neonConfig.webSocketConstructor = WebSocket;
 
 const adapter = new PrismaNeon({
   connectionString: process.env.DATABASE_URL!,
